50fa426a6d913d61cf5376258103072013802c3a
Joonyoung <blackhound97@gmail.com>
2023-09-25 22:51:52 +0900

    기본 프론트엔드

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const path = require("path");
-const session = require("./routes/page");
+const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 
@@ -19,11 +19,10 @@ nunjucks.configure("views", {
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
+app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
@@ -31,8 +30,7 @@ app.use(
       httpOnly: true,
       secure: false,
     },
-  })
-);
+  }));
 
 app.use("/", pageRouter);
 
@@ -43,10 +41,10 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.locals.message = error.message;
+  res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render(error);
+  res.render(err);
 });
 
 app.listen(app.get("port"), () => {
