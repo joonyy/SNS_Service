78b815ed05dee49e76beed76c784d3472197c25b
Joonyoung <blackhound97@gmail.com>
2023-09-28 19:06:19 +0900

    이미지 업로드 구현 - 테스트 안됨

--- controllers/page.js
+++ controllers/page.js
@@ -1,3 +1,5 @@
+const {User, Post} = require('../models');
+
 exports.renderProfile = (req,res) =>{
   res.render('profile', {title: '내정보 - NodeBird'});
 };
@@ -6,10 +8,21 @@ exports.renderJoin = (req,res) =>{
   res.render('join', {title:'회원가입 - NodeBird'});
 };
 
-exports.renderMain = (req,res, next) =>{
-  const twits = [];
+exports.renderMain = async (req,res, next) =>{
+  try{
+    const posts = await Post.findAll({
+      include:{
+        model:User,
+        attributes:['id', 'nick'],
+      },
+      order:[['createdAt','DESC']],
+    });
   res.render('main', {
     title : 'NodeBird',
-    twits,
+    twits : posts,
   });
-};
+  }catch(err){
+    console.error(err);
+    next(err);
+  }
+}
