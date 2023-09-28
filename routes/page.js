0fcd061b2e12888560babc02ccfa82803ef1f67d
Joonyoung <blackhound97@gmail.com>
2023-09-28 18:40:15 +0900

    카카오 인증. 오류 있음

--- routes/page.js
+++ routes/page.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.use((req,res,next)=>{
   res.locals.user= req.user;
-  res.locals.user = null;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followingIdList = [];
